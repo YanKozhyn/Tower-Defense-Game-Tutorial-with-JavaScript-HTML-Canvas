6ff178fbdc0530514f2951e7487e3c848263a7f3
JanHappy <yan.kozhyn@gmail.com>
2022-08-22 21:02:15 +0300

    complete shooting projectiles

--- js/classes.js
+++ js/classes.js
@@ -35,10 +35,14 @@ class Enemy {
       x: this.position.x + this.width / 2,
       y: this.position.y + this.height / 2,
     };
+    this.radius = 50;
   }
   draw() {
     ctx.fillStyle = 'red';
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.beginPath();
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
   }
   update() {
     this.draw();
@@ -64,15 +68,80 @@ class Enemy {
   }
 }
 
+class Projectile {
+  constructor({ position = { x: 0, y: 0 }, enemy }) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.enemy = enemy;
+    this.radius = 10;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+
+    const angle = Math.atan2(
+      this.enemy.center.y - this.position.y,
+      this.enemy.center.x - this.position.x
+    );
+
+    const power = 2.5;
+    this.velocity.x = Math.cos(angle) * power;
+    this.velocity.y = Math.sin(angle) * power;
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 class Building {
   constructor({ position = { x: 0, y: 0 } }) {
     this.position = position;
     this.width = 64 * 2;
     this.height = 64;
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2,
+    };
+    this.projectiles = [];
+    this.radius = 250;
+    this.target;
+    this.frames = 0;
   }
 
   draw() {
     ctx.fillStyle = 'blue';
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    ctx.beginPath();
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'rgba(0,0,255,0.2)';
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    if (this.frames % 200 === 0 && this.target) {
+      this.projectiles.push(
+        new Projectile({
+          position: {
+            x: this.center.x,
+            y: this.center.y,
+          },
+          enemy: this.target,
+        })
+      );
+    }
+
+    this.frames++;
   }
 }
